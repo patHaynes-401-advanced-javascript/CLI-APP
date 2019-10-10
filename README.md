b9517fa1af581de34a49f5d74333bc8a38adada8
Pat Haynes <pathaynes32@gmail.com>
2019-10-09 18:01:24 -0700

    tryin to get tests going

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CLI-APP
+# Socket.io
+
---
+++ app/__tests__/functions.test.js
@@ -0,0 +1,18 @@
+const uppercase = require('../functions');
+
+describe('Do the socket functions work?', () => {
+
+  it('turns a string into uppercase', () => {
+    expect(uppercase('sadmodude')).toBe('SADMODUDE');
+  });
+
+  it('reads a file', () => {
+
+  });
+
+  it('writes a file', () => {
+
+  });
+
+
+});
